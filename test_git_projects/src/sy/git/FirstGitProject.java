6ca67167a60f835511edabea819583b038cdef87
shenla <1093911042@qq.com>
2018-11-21 21:06:41 +0800

    clone up

--- test_git_projects/src/sy/git/FirstGitProject.java
+++ test_git_projects/src/sy/git/FirstGitProject.java
@@ -6,10 +6,12 @@ public class FirstGitProject {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //git test
     }
 
     public void test() {
         int i = 1;
         System.out.println(i);
+        System.out.println(i + 2);
     }
 }
