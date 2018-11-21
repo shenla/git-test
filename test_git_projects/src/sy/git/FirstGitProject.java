cc916da443a01a3f235565bbca8ae548ee051c42
shenla <1093911042@qq.com>
2018-11-21 20:34:14 +0800

    first test git project

---
+++ test_git_projects/src/sy/git/FirstGitProject.java
@@ -0,0 +1,10 @@
+package sy.git;
+
+public class FirstGitProject {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
